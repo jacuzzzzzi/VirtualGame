6d322b65e01306817b93b34866f12418fce76957
jacuzzzzzi <jacuzzzzzi@gmail.com>
2025-04-11 20:55:57 +0900

    test

---
+++ src/env.py
@@ -0,0 +1,63 @@
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_teddynote import logging
+logging.langsmith("VirtualGameEnv")
+import os
+
+openai_api_key = os.environ('OPENAI_API_KEY')
+
+generate_prompt_path = '../prompt/generate_prompt'
+
+with open(generate_prompt_path,'r') as f:
+    generate_prompt = f.read()
+
+prompt = PromptTemplate(
+    template=generate_prompt,
+    input_variables=["country1"],
+)
+
+game_state = {
+    "characters": [],
+    "victim": None,
+    "culprits": [],   # list of names
+    "clues": [],
+    "solution_explanation": ""
+}
+
+
+def character_generate(prompt):
+    input = prompt
+    llm = ChatOpenAI(api_key = openai_api_key, model = "gpt-4o-mini")
+    llm.invoke(input)
+
+class Character:
+    def __init__(self, name, traits, personality, alibi, secret):
+        self.name = name
+        self.traits = traits        # superficial description
+        self.personality = personality  # deeper details or motive
+        self.alibi = alibi          # what they'll claim
+        self.secret = secret        # e.g. "Culprit" or other hidden info
+
+game_state = {
+    "characters": [],
+    "victim": None,
+    "culprits": [],   # list of names
+    "clues": [],
+    "solution_explanation": ""
+}
+
+
+class agonist():
+    def __init__(self, hp=100, money=100):
+        self.hp = hp
+        self.money = money
+
+class npc():
+    def __init__(self, name):
+        self.name = name
+
+# class event():
+#     def __init__(self):
+
+
+# class game_env():
